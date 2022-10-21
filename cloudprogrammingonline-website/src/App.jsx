5edaeb168b0c1212dabcce1b625f2090dd067ad9
pangsitmie <66163611+pangsitmie@users.noreply.github.com>
2022-10-21 18:01:07 +0800

    change icon and create service component

--- cloudprogrammingonline-website/src/App.jsx
+++ cloudprogrammingonline-website/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
 import RecentProject from './components/recentProject/RecentProject'
+import Service from './components/service/Service'
 // shortcut is "rafce"
 //header already contains cloud3d, nav, and hero components
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
         <>
             <Header />
             <RecentProject />
+            <Service />
         </>
     )
 }
---
+++ cloudprogrammingonline-website/src/components/service/Service.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './service.css'
+const Service = () => {
+    return (
+        <div className='service_container'>
+            <div className="container ">
+                <h1 className='service-title'>我們正在做什麼，以及我們公司如何幫助您改善業務</h1>
+            </div>
+
+            <div className="container">
+                <h2 className='description' >
+                    羅位陽育些常料現、東女相提兒遠：特的外才刻東是收血。除座去重定們生它行裡師福動原效關一、張前電香物了投時。有開立養麼書頭年萬人過治世的市次相家，和老電她不、
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default Service
