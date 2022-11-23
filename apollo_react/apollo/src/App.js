d40299d89b5513fab971d9624e55731e2ab5d12d
pangsitmie <66163611+pangsitmie@users.noreply.github.com>
2022-11-23 17:49:22 +0800

    add gql package

--- apollo_react/apollo/src/App.js
+++ apollo_react/apollo/src/App.js
@@ -2,13 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
 import Homepage from './pages/Homepage';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Homepage />} />
-        {/* <Route path='/login' element={<Homepage />} /> */}
+        <Route path='/login' element={<Login />} />
 
       </Routes>
     </div>
