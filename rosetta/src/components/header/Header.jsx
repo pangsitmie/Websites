05a363c0e42ee3c83ee90354eb8f305bac3dfc1b
pangsitmie <pangsitmie@github.com>
2023-01-14 23:06:22 +0800

    fix header

--- rosetta/src/components/header/Header.jsx
+++ rosetta/src/components/header/Header.jsx
@@ -68,74 +68,72 @@ const Header = () => {
     }
 
     return (
-        <header className="header__middle">
-            <div className="header_container">
-                <div className="row">
-                    {/* Add Logo  */}
-                    <div className="header__middle__logo">
-                        <a href="/"><img src={ROSETTA_LOGO} alt="logo" className='header_icon_img' /></a>
-                    </div>
+        <header className="header_container">
+            <div className="row">
+                {/* Add Logo  */}
+                <div className="header__middle__logo">
+                    <a href="/">
+                        <img src={ROSETTA_LOGO} alt="logo" className='header_icon_img' />
+                    </a>
+                </div>
 
-                    <div className="header__middle__menus">
-                        <nav className="main-nav " >
-                            {/* Responsive Menu Button */}
-                            {isResponsiveclose === true ? <>
-                                <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiXCircle />   </span>
-                            </> : <>
-                                <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiAlignRight />   </span>
-                            </>}
+                <div className="header__middle__menus">
+                    <nav className="main-nav " >
+                        {/* Responsive Menu Button */}
+                        {isResponsiveclose === true ? <>
+                            <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiXCircle />   </span>
+                        </> : <>
+                            <span className="menubar__button" style={{ display: 'none' }} onClick={toggleClass} > <FiAlignRight />   </span>
+                        </>}
 
 
-                            <ul className={boxClass.join(' ')}>
-                                {/* <li className="menu-item" >
+                        <ul className={boxClass.join(' ')}>
+                            {/* <li className="menu-item" >
                                     <NavLink exact activeClassName='is-active' onClick={toggleClass} to={`/`}> Home </NavLink>
                                 </li> */}
 
-                                {/* 商務合作 MENU ITEM */}
-                                <li onClick={toggleSubmenu1} className="menu-item sub__menus__arrows" > <Link to="#">Products<FiChevronDown /> </Link>
-                                    <ul className={boxClassSubMenu1.join(' ')} >
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Personalize Product Recommenders</NavLink> </li>
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>AI Exit-intent Promotions</NavLink> </li>
-                                    </ul>
-                                </li>
+                            {/* 商務合作 MENU ITEM */}
+                            <li onClick={toggleSubmenu1} className="menu-item sub__menus__arrows" > <Link to="#">Products<FiChevronDown /> </Link>
+                                <ul className={boxClassSubMenu1.join(' ')} >
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Personalize Product Recommenders</NavLink> </li>
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>AI Exit-intent Promotions</NavLink> </li>
+                                </ul>
+                            </li>
 
-                                <li className="menu-item" ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Technology</NavLink> </li>
-
-
-                                {/* 服務平台 MENU ITEM */}
-                                <li onClick={toggleSubmenu2} className="menu-item sub__menus__arrows" > <Link to="#">Resources<FiChevronDown /> </Link>
-                                    <ul className={boxClassSubMenu2.join(' ')} >
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Case Studies</NavLink> </li>
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>MarTech Blog</NavLink> </li>
-                                    </ul>
-                                </li>
-
-                                <li className="menu-item" ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Pricing</NavLink> </li>
+                            <li className="menu-item" ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Technology</NavLink> </li>
 
 
-                                <li onClick={toggleSubmenu2} className="menu-item sub__menus__arrows" > <Link to="#">About Us<FiChevronDown /> </Link>
-                                    <ul className={boxClassSubMenu2.join(' ')} >
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Press</NavLink> </li>
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>About</NavLink> </li>
-                                        <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Partners</NavLink> </li>
-                                    </ul>
-                                </li>
+                            {/* 服務平台 MENU ITEM */}
+                            <li onClick={toggleSubmenu2} className="menu-item sub__menus__arrows" > <Link to="#">Resources<FiChevronDown /> </Link>
+                                <ul className={boxClassSubMenu2.join(' ')} >
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Case Studies</NavLink> </li>
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>MarTech Blog</NavLink> </li>
+                                </ul>
+                            </li>
+
+                            <li className="menu-item" ><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Pricing</NavLink> </li>
 
 
-
-                                <li onClick={toggleSubmenu4} className="menu-item sub__menus__arrows" > <Link to="#"><TbWorld /><br /><FiChevronDown /> </Link>
-                                    <ul className={boxClassSubMenu4.join(' ')} >
-                                        <li> <NavLink activeClassName='is-active'>EN</NavLink> </li>
-                                        <li><NavLink activeClassName='is-active'>中文</NavLink> </li>
-                                    </ul>
-                                </li>
-                            </ul>
-
-
-                        </nav>
-                    </div>
-
+                            <li onClick={toggleSubmenu2} className="menu-item sub__menus__arrows" > <Link to="#">About Us<FiChevronDown /> </Link>
+                                <ul className={boxClassSubMenu2.join(' ')} >
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Press</NavLink> </li>
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>About</NavLink> </li>
+                                    <li><NavLink onClick={toggleClass} activeClassName='is-active' to={`/`}>Partners</NavLink> </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
                 </div>
+
+                <div className='header_login_container'>
+                    <button className='btn_transparent'>
+                        Login
+                    </button>
+                    <button className='btn_header_pink btn_margin_left'>
+                        Get Started
+                    </button>
+                </div>
+
             </div>
         </header>
     )
