5bff4e36efe52820ffa63761d29d608324e7ef97
pangsitmie <66163611+pangsitmie@users.noreply.github.com>
2023-01-16 10:54:03 +0800

    fix login page

--- winpro_react_admin/src/scenes/login/Login.jsx
+++ winpro_react_admin/src/scenes/login/Login.jsx
@@ -92,77 +92,164 @@ const Login = () => {
 
 
     // ========================== RETURN ==========================
+    // return (
+    //     <Box className='login_page' backgroundColor={colors.primary[400]} >
+    //         <Box p="6rem 2.5rem" className='login_container'>
+    //             <Typography variant="h1" sx={{ mb: "30px", textAlign: "center", fontWeight: "600", color: "white" }}>
+    //                 Game Pay
+    //             </Typography>
+
+    //             <Formik
+    //                 onSubmit={handleFormSubmit}
+    //                 initialValues={initialValues}
+    //                 validationSchema={checkoutSchema}
+    //             >
+    //                 {({
+    //                     values,
+    //                     errors,
+    //                     touched,
+    //                     handleBlur,
+    //                     handleChange,
+    //                     handleSubmit,
+    //                 }) => (
+    //                     <form onSubmit={handleSubmit}>
+    //                         <Box >
+    //                             <TextField
+    //                                 fullWidth
+    //                                 variant="filled"
+    //                                 type="text"
+    //                                 label="Account"
+    //                                 onBlur={handleBlur}
+    //                                 onChange={handleChange}
+    //                                 value={values.account}
+    //                                 name="account"
+    //                                 error={!!touched.account && !!errors.account}
+    //                                 helperText={touched.account && errors.account}
+    //                                 sx={{ marginBottom: "1rem", mr: "1rem", backgroundColor: "#1F2A40", borderRadius: "5px" }}
+    //                             />
+
+    //                             {/* PASSWORD INPUT */}
+    //                             <FormControl fullWidth variant="filled" sx={{ marginBottom: "1rem", backgroundColor: "#1F2A40", borderRadius: "5px" }} >
+    //                                 <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+    //                                 <FilledInput
+    //                                     onBlur={handleBlur}
+    //                                     onChange={handleChange}
+    //                                     value={values.password}
+    //                                     name="password"
+    //                                     error={!!touched.password && !!errors.password}
+    //                                     type={showPassword ? 'text' : 'password'}
+    //                                     endAdornment={
+    //                                         <InputAdornment position="end">
+    //                                             <IconButton
+    //                                                 aria-label="toggle password visibility"
+    //                                                 onClick={handleClickShowPassword}
+    //                                                 onMouseDown={handleMouseDownPassword}
+    //                                                 edge="end"
+    //                                             >
+    //                                                 {showPassword ? <VisibilityOff /> : <Visibility />}
+    //                                             </IconButton>
+    //                                         </InputAdornment>
+    //                                     }
+    //                                 />
+    //                                 <FormHelperText error={!!touched.password && !!errors.password}>
+    //                                     {touched.password && errors.password}
+    //                                 </FormHelperText>
+    //                             </FormControl>
+    //                         </Box>
+    //                         <Box display="flex" justifyContent="center" paddingTop={"2rem"}>
+    //                             <button className="my-button" type="submit" >Login</button>
+    //                         </Box>
+    //                     </form>
+    //                 )}
+    //             </Formik>
+    //         </Box >
+    //     </Box>
+    // )
+
+
+
     return (
         <Box className='login_page' backgroundColor={colors.primary[400]} >
-            <Box p="6rem 2.5rem" className='login_container'>
-                <Typography variant="h1" sx={{ mb: "30px", textAlign: "center", fontWeight: "600", color: "white" }}>
-                    Game Pay
-                </Typography>
+            <div class="container">
+                <div class="box">
+                    <span class="title">GAME PAY <br />LOGIN</span>
+                    <div>
+                        <Formik
+                            onSubmit={handleFormSubmit}
+                            initialValues={initialValues}
+                            validationSchema={checkoutSchema}
+                        >
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleBlur,
+                                handleChange,
+                                handleSubmit,
+                            }) => (
+                                <form onSubmit={handleSubmit}>
+                                    <Box >
+                                        <TextField
+                                            fullWidth
+                                            variant="filled"
+                                            type="text"
+                                            label="Account"
+                                            onBlur={handleBlur}
+                                            onChange={handleChange}
+                                            value={values.account}
+                                            name="account"
+                                            error={!!touched.account && !!errors.account}
+                                            helperText={touched.account && errors.account}
+                                            sx={{ marginBottom: "1rem", mr: "1rem", borderRadius: "5px" }}
+                                        />
 
-                <Formik
-                    onSubmit={handleFormSubmit}
-                    initialValues={initialValues}
-                    validationSchema={checkoutSchema}
-                >
-                    {({
-                        values,
-                        errors,
-                        touched,
-                        handleBlur,
-                        handleChange,
-                        handleSubmit,
-                    }) => (
-                        <form onSubmit={handleSubmit}>
-                            <Box >
-                                <TextField
-                                    fullWidth
-                                    variant="filled"
-                                    type="text"
-                                    label="Account"
-                                    onBlur={handleBlur}
-                                    onChange={handleChange}
-                                    value={values.account}
-                                    name="account"
-                                    error={!!touched.account && !!errors.account}
-                                    helperText={touched.account && errors.account}
-                                    sx={{ marginBottom: "1rem", mr: "1rem", backgroundColor: "#1F2A40", borderRadius: "5px" }}
-                                />
+                                        {/* PASSWORD INPUT */}
+                                        <FormControl fullWidth variant="filled" sx={{ marginBottom: "1rem", borderRadius: "5px" }} >
+                                            <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
+                                            <FilledInput
+                                                onBlur={handleBlur}
+                                                onChange={handleChange}
+                                                value={values.password}
+                                                name="password"
+                                                error={!!touched.password && !!errors.password}
+                                                type={showPassword ? 'text' : 'password'}
+                                                endAdornment={
+                                                    <InputAdornment position="end">
+                                                        <IconButton
+                                                            aria-label="toggle password visibility"
+                                                            onClick={handleClickShowPassword}
+                                                            onMouseDown={handleMouseDownPassword}
+                                                            edge="end"
+                                                        >
+                                                            {showPassword ? <VisibilityOff /> : <Visibility />}
+                                                        </IconButton>
+                                                    </InputAdornment>
+                                                }
+                                            />
+                                            <FormHelperText error={!!touched.password && !!errors.password}>
+                                                {touched.password && errors.password}
+                                            </FormHelperText>
+                                        </FormControl>
+                                    </Box>
+                                    <Box display="flex" justifyContent="center" paddingTop={"2rem"}>
+                                        {/* <button className="my-button" type="submit" >Login</button> */}
+                                        <button className='btn_right_arrow' type="submit">
+                                            Login
+                                            <div class="arrow-wrapper">
+                                                <div class="arrow"></div>
 
-                                {/* PASSWORD INPUT */}
-                                <FormControl fullWidth variant="filled" sx={{ marginBottom: "1rem", backgroundColor: "#1F2A40", borderRadius: "5px" }} >
-                                    <InputLabel htmlFor="filled-adornment-password">Password</InputLabel>
-                                    <FilledInput
-                                        onBlur={handleBlur}
-                                        onChange={handleChange}
-                                        value={values.password}
-                                        name="password"
-                                        error={!!touched.password && !!errors.password}
-                                        type={showPassword ? 'text' : 'password'}
-                                        endAdornment={
-                                            <InputAdornment position="end">
-                                                <IconButton
-                                                    aria-label="toggle password visibility"
-                                                    onClick={handleClickShowPassword}
-                                                    onMouseDown={handleMouseDownPassword}
-                                                    edge="end"
-                                                >
-                                                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                                                </IconButton>
-                                            </InputAdornment>
-                                        }
-                                    />
-                                    <FormHelperText error={!!touched.password && !!errors.password}>
-                                        {touched.password && errors.password}
-                                    </FormHelperText>
-                                </FormControl>
-                            </Box>
-                            <Box display="flex" justifyContent="center" paddingTop={"2rem"}>
-                                <button className="my-button" type="submit" >Login</button>
-                            </Box>
-                        </form>
-                    )}
-                </Formik>
-            </Box >
+                                            </div>
+                                        </button>
+                                    </Box>
+                                </form>
+                            )}
+                        </Formik>
+                    </div>
+                    <div>
+                        <span>雲程在線</span>
+                    </div>
+                </div>
+            </div>
         </Box>
     )
 };
